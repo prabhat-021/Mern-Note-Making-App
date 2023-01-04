e4858fc40d646e5f3ac08c52e1f996cdcd46fae3
prabhat_021 <prabhatsahrawat010203@gmail.com>
2023-01-05 01:04:17 +0530

    landing page done

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,12 +1,28 @@
 import "./LandingPage.css";
-import { Container, Row } from "react-bootstrap";
+import { Button, Container, Row } from "react-bootstrap";
 
 export default function LandingPage() {
     return (
         <section className="main">
             <Container>
                 <Row>
-                    LandingPage
+                    <div className="intro-text">
+                        <h1 className="title">Welcome to Note-Making App</h1>
+                        <p className="sub-title">One safe Place for all your Notes </p>
+                        <div className="button">
+                            <a href="/login">
+                                <Button size="lg" className="landingbutton mr-4" >
+                                    Login
+                                </Button>
+                            </a>
+                            <a href="/register">
+                                <Button size="lg" className="landingbutton ml-4" variant="outline-primary" >
+                                    Signup
+                                </Button>
+                            </a>
+                        </div>
+                    </div>
+
                 </Row>
             </Container>
         </section>
